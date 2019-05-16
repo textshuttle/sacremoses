09054c78c362821b824520ef44b72a1ceb7d411b
Patrick Düggelin <patrick.dueggelin@live.de>
2019-05-16 10:22:54 +0200

    fix comma no-break space regression
    Also added clearer no-break spaces

--- sacremoses/normalize.py
+++ sacremoses/normalize.py
@@ -32,11 +32,11 @@ class MosesPunctuationNormalizer:
     https://github.com/moses-smt/mosesdecoder/blob/master/scripts/tokenizer/normalize-punctuation.perl
     """
 
-    SUBSTITUTIONS_EXTRA_WHITESPACE = [ # lines 21 - 30
+    SUBSTITUTIONS_EXTRA_WHITESPACE = [  # lines 21 - 30
         substitute('\r', ''),
         substitute('(', ' ('),
         substitute(')', ') '),
-        substitute_regex(r' +', r' '),
+        substitute_regex(r' +', r' '),  # TODO why twice? see line 90
         substitute_regex(r'\) ([.!:?;,])', r')\g<1>'),
         substitute('( ', '('),
         substitute(' )', ')'),
@@ -45,12 +45,12 @@ class MosesPunctuationNormalizer:
         substitute(' ;', ';'),
     ]
 
-    SUBSTITUTIONS_NORMALIZE_UNICODE_IF_NOT_PENN = [ # lines 33 - 34
+    SUBSTITUTIONS_NORMALIZE_UNICODE_IF_NOT_PENN = [  # lines 33 - 34
         substitute('`', "'"),
         substitute("''", ' " '),
     ]
 
-    SUBSTITUTIONS_NORMALIZE_UNICODE = [ # lines 37 - 50
+    SUBSTITUTIONS_NORMALIZE_UNICODE = [  # lines 37 - 50
         substitute('„', '"'),
         substitute('“', '"'),
         substitute('”', '"'),
@@ -62,31 +62,33 @@ class MosesPunctuationNormalizer:
         substitute_regex(r'([a-z])’([a-z])', r"\g<1>'\g<2>", ignore_case=True),
         substitute('‘', '"'),
         substitute('‚', '"'),
+        # TODO this can cause problems. (In some texts right single quotation marks are used as apostrophes)
+        # use regex to look for matching opening quot?
         substitute('’', '"'),
         substitute("''", '"'),
         substitute("´´", '"'),
         substitute("…", "..."),
     ]
 
-    SUBSTITUTIONS_FRENCH_QUOTES = [ # lines 52 - 57
-        substitute(' « ', ' "'),
-        substitute('« ', '"'),
+    SUBSTITUTIONS_FRENCH_QUOTES = [  # lines 52 - 57
+        substitute('\N{NO-BREAK SPACE}«\N{NO-BREAK SPACE}', ' "'),
+        substitute('«\N{NO-BREAK SPACE}', '"'),
         substitute('«', '"'),
-        substitute(' » ', '" '),
-        substitute(' »', '"'),
+        substitute('\N{NO-BREAK SPACE}»\N{NO-BREAK SPACE}', '" '),
+        substitute('\N{NO-BREAK SPACE}»', '"'),
         substitute('»', '"'),
     ]
 
-    SUBSTITUTIONS_HANDLE_PSEUDO_SPACES = [ # lines 59 - 67
-        substitute(' %', '%'),
-        substitute('nº ', 'nº '),
-        substitute(' :', ':'),
-        substitute(' ºC', ' ºC'),
-        substitute(' cm', ' cm'),
-        substitute(' ?', '?'),
-        substitute(' !', '!'),
-        substitute(' ;', ';'),
-        substitute(', ', ','),
+    SUBSTITUTIONS_HANDLE_PSEUDO_SPACES = [  # lines 59 - 67
+        substitute('\N{NO-BREAK SPACE}%', '%'),
+        substitute('nº\N{NO-BREAK SPACE}', 'nº '),
+        substitute('\N{NO-BREAK SPACE}:', ':'),
+        substitute('\N{NO-BREAK SPACE}ºC', ' ºC'),
+        substitute('\N{NO-BREAK SPACE}cm', ' cm'),
+        substitute('\N{NO-BREAK SPACE}?', '?'),
+        substitute('\N{NO-BREAK SPACE}!', '!'),
+        substitute('\N{NO-BREAK SPACE};', ';'),
+        substitute(',\N{NO-BREAK SPACE}', ', '),
         substitute_regex(r' +', r' '),
     ]
 
@@ -96,7 +98,7 @@ class MosesPunctuationNormalizer:
 
     SUBSTITUTIONS_DE_ES_FR_QUOTATION_FOLLOWED_BY_COMMA = [
         substitute(',"', '",'),
-        substitute_regex(r'(\.+)"(\s*[^<])', r'"\g<1>\g<2>'), # don't fix period at end of sentence
+        substitute_regex(r'(\.+)"(\s*[^<])', r'"\g<1>\g<2>'),  # don't fix period at end of sentence
     ]
 
     SUBSTITUTIONS_DE_ES_CZ_CS_FR = [
